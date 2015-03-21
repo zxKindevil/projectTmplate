4036f87a23ef66ff04faad2495df6424f4a1c33b
Kindevil <zxkindevil@163.com>
2015-03-22 00:05:47 +0800

    guavaTemplate

---
+++ src/main/java/com/zhangxin/lguava/App.java
@@ -0,0 +1,13 @@
+package com.zhangxin.lguava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
